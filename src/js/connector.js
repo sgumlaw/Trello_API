16d206ecd5e8a06b4dc005a6a48e4a1ebca55e03
sgumlaw <sgumlaw@live.com>
2020-03-27 21:34:44 -0400

    Fetch weather data for Trello cards

--- src/js/connector.js
+++ src/js/connector.js
@@ -3,12 +3,26 @@ console.log('Hello World');
 window.TrelloPowerUp.initialize({
     'card-badges': function (t, opts) {
         // return an array of card badges for the given card
-        return t.card('all')
+        return t.card('coordinates')
             .then(function (card) {
                 console.log(card);
-                return [{
-                    text: card.idShort
-                }];
+                if (card.coordinates) {
+                    const { latitude, longitude } = card.coordinates;
+                    //if our card has a location, we will pull the current weather data
+                    return fetch('https://http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&
+                    APPID =%% APP_ID %% ')
+                            .then(function (response) {
+                                return response.json();
+                            })
+                            .then(function (weaterData) {
+                                return [{
+                                    text: weatherData.main.temp.toString()
+                                }, {
+                                    text: weatherData.main.wind.speed.toString()
+                                }]
+                            });
+                }
+                return [];
             })
     }
 });
